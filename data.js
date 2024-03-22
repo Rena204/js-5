f2e368c76d2f7f27c67796e53296aa33bb6cec0c
rena204 <rena2004.rr@gamil.com>
2024-03-22 22:56:45 +0400

    js5

---
+++ data.js
@@ -0,0 +1,17 @@
+var jsonData = {
+    "title": "Список фильмов",
+    "movies": [
+        {
+            "id": 1,
+            "title": "Звездные войны",
+            "poster": "https://example.com/poster1.jpg",
+            "description": "Эпическая космическая опера"
+        },
+        {
+            "id": 2,
+            "title": "Гарри Поттер",
+            "poster": "https://example.com/poster2.jpg",
+            "description": "Фэнтезийная сага о волшебнике"
+        }
+    ]
+};
